0a2553917d82dbb51b7f94ca2d8326c210565630
Norman <jamesgreennorman@gmail.com>
2025-08-28 00:53:24 +1000

    Added Icon with differing sizes

--- src/main/java/com/cab302/bugco/HomeApplication.java
+++ src/main/java/com/cab302/bugco/HomeApplication.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HomeApplication extends Application {
     // Constants defining the window title and size
@@ -18,7 +20,15 @@ public class HomeApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         scene.getStylesheets().add(
-                getClass().getResource("styles.css").toExternalForm()
+                Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm()
+        );
+        String iconUrl = Objects.requireNonNull(getClass().getResource("image.png")).toExternalForm();
+        stage.getIcons().addAll(
+                new Image(iconUrl, 16, 16, true, true),
+                new Image(iconUrl, 32, 32, true, true),
+                new Image(iconUrl, 48, 48, true, true),
+                new Image(iconUrl, 128, 128, true, true),
+                new Image(iconUrl, 256, 256, true, true)
         );
         stage.setTitle(TITLE);
         stage.setScene(scene);
